1ecc4f1068af8bf4cfe403ddace8ebfbf7918d5b
harry2992 <blackcigar2992@gmail.com>
2024-08-17 01:45:19 +0700

    - Refactor authentication route handlers into async functions - Add authentication error responses

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var db = require('../database/db')
 var crypto = require('crypto')
-var util = require('util')
+var util = require('util');
 
 var rawParser = bodyParser.raw();
 var textParser = bodyParser.text();
@@ -29,78 +29,84 @@ const secureConnection = (req, res, next) => {
 
   if (clientPublicJwk && serverPublicJwk && serverPrivateJwk)
     next()
-  else res.status(428).send()
+  else res.status(428).end()
 }
 
-const decryptRequestBody = (req, res, next) => {
-  subtle.importKey("jwk", req.session.serverPrivateJwk, rsa, true, ["decrypt"])
-    .then(serverPrivateKey => subtle.decrypt(rsa, serverPrivateKey, req.body))
-    .then(decryptedBodyAb => {
-      const bodyStr = new TextDecoder().decode(decryptedBodyAb)
-      req.decryptedBody = JSON.parse(bodyStr)
-      next()
-    })
+const decryptRequestBody = async (req, res, next) => {
+  const serverPrivateKey = await subtle.importKey("jwk", req.session.serverPrivateJwk, rsa, true, ["decrypt"])
+  const decryptedBodyAb = await subtle.decrypt(rsa, serverPrivateKey, req.body)
+  const bodyStr = new TextDecoder().decode(decryptedBodyAb)
+  req.decryptedBody = JSON.parse(bodyStr)
+  next()
 }
 
-router.post('/exchangePublicKey', textParser, function (req, res, next) {
-  if (!req.body) throw new Error('Client public key not found.')
-  subtle.generateKey(rsaKeyGeneration, true, ["encrypt", "decrypt"])
-    .then(({ publicKey, privateKey }) => {
-      const serverPublicJwkPromise = subtle.exportKey("jwk", publicKey)
-      const serverPrivateJwkPromise = subtle.exportKey("jwk", privateKey)
-      return Promise.all([serverPublicJwkPromise, serverPrivateJwkPromise])
-    })
-    .then((jwks) => {
-      req.session.serverPublicJwk = jwks[0]
-      req.session.serverPrivateJwk = jwks[1]
-      req.session.clientPublicJwk = req.body
-      res.send(req.session.serverPublicJwk)
-    })
+router.post('/exchangePublicKey', textParser, async function (req, res, next) {
+  if (!req.body) {
+    res.status(422).send('Missing client public key.')
+    return
+  }
+  const { publicKey, privateKey } = await subtle.generateKey(rsaKeyGeneration, true, ["encrypt", "decrypt"])
+  const serverPublicJwk = await subtle.exportKey("jwk", publicKey)
+  const serverPrivateJwk = await subtle.exportKey("jwk", privateKey)
+  req.session.serverPublicJwk = serverPublicJwk
+  req.session.serverPrivateJwk = serverPrivateJwk
+  req.session.clientPublicJwk = req.body
+  res.cookie('serverPublicJwkStr', JSON.stringify(serverPublicJwk), { expires: req.session.cookie.expires })
+  res.end()
 })
 
-router.post('/signup', [secureConnection, rawParser, decryptRequestBody], function (req, res, next) {
+router.post('/signup', [secureConnection, rawParser, decryptRequestBody], async function (req, res, next) {
   let account = req.decryptedBody
-  account.salt = crypto.randomUUID()
-  let createdAccount = null
-
-  pbkdf2Async(account.password, account.salt, 2000, 64, 'SHA-256')
-    .then(hashedPassword => {
-      account.hashedPassword = hashedPassword
-      return db.connect()
-    })
-    .then(() => {
-      return db.Account.create([{
-        username: account.username,
-        email: account.email,
-        salt: account.salt,
-        password: account.hashedPassword,
-        status: 'Created'
-      }])
-    })
-    .then(docs => {
-      createdAccount = {
-        id: docs[0].id,
-        username: docs[0].username,
-        email: docs[0].email,
-        createdDate: docs[0].createdDate,
-        updatedDate: docs[0].updatedDate,
-        status: docs[0].status
-      }
-      return db.disconnect()
-    })
-    .then(() => encrypt(createdAccount, req.session.clientPublicJwk))
-    .then(encryptedAccount => {
-      req.session.account = createdAccount
-      req.session.isLogedIn = true
-      req.session.maxAge = req.session.originalMaxAge
-      res.send(Buffer.from(encryptedAccount))
-    })
+  const salt = crypto.randomUUID()
+  const hashedPassword = await pbkdf2Async(account.password, salt, 2000, 64, 'SHA-256')
+  await db.connect()
+  try {
+    var docs = await db.Account.create([{
+      username: account.username,
+      email: account.email,
+      salt: salt,
+      password: hashedPassword,
+      status: 'Created'
+    }])
+  } catch (err) {
+    res.status(409)
+    if (err.keyValue.email)
+      res.send('Email is already in use')
+    else if (err.keyValue.username)
+      res.send('Username is already in use')
+    else res.send('Cannot create new account')
+    return
+  }
+  await db.disconnect()
+  const createdAccount = {
+    id: docs[0].id,
+    username: docs[0].username,
+    email: docs[0].email,
+    createdDate: docs[0].createdDate,
+    updatedDate: docs[0].updatedDate,
+    status: docs[0].status
+  }
+  const encryptedAccount = await encrypt(createdAccount, req.session.clientPublicJwk)
+  req.session.account = createdAccount
+  req.session.isLogedIn = true
+  req.session.maxAge = req.session.originalMaxAge
+  res.send(Buffer.from(encryptedAccount))
 })
 
 router.post('/login', [secureConnection, rawParser, decryptRequestBody], async function (req, res, next) {
   const loginInfo = req.decryptedBody
-  if (!loginInfo || !loginInfo.account || !loginInfo.password)
-    throw new Error('Missing login information.')
+  if (!loginInfo) {
+    res.status(422).send('Missing login info')
+    return
+  }
+  else if (!loginInfo.account) {
+    res.status(422).send('Missing email or username')
+    return
+  }
+  else if (!loginInfo.password) {
+    res.status(422).send('Missing password')
+    return
+  }
 
   await db.connect()
   const account = await db.Account.findOne({
@@ -109,11 +115,17 @@ router.post('/login', [secureConnection, rawParser, decryptRequestBody], async f
       { username: loginInfo.account }
     ]
   })
-  if (account == null) throw new Error('Account not found.')
+  await db.disconnect()
+  if (account == null) {
+    res.status(401).send('Incorrect account')
+    return
+  }
   const hashedPassword = await pbkdf2Async(loginInfo.password, account.salt, 2000, 64, 'SHA-256')
 
-  if (Buffer.compare(hashedPassword, account.password) != 0)
-    res.status(401).send('Incorrect password!')
+  if (Buffer.compare(hashedPassword, account.password) != 0) {
+    res.status(401).send('Incorrect password')
+    return
+  }
 
   const accountInfo = {
     id: account.id,
@@ -131,15 +143,29 @@ router.post('/login', [secureConnection, rawParser, decryptRequestBody], async f
 })
 
 router.post('/logout', function (req, res, next) {
+  res.clearCookie()
   req.session.destroy((err) => {
-    if (err) throw new Error(err)
-    res.send('Bye bye')
+    if (err) next(err)
+    res.end()
   })
 })
 
-router.get('/isLogedIn', function (req, res, next) {
-  if (req.session.isLogedIn) res.send(true)
-  else res.send(false)
+router.get('/isLogedIn', async function (req, res, next) {
+  // if (req.session.isLogedIn) {
+  //   await db.connect()
+  //   const account = await db.Account.findById(req.session.account.id)
+  //   await db.disconnect()
+  //   if (account == null) {
+  //     res.status(401).send('Unable to retrieve account info.')
+  //   } else {
+  //     req.session.account = account
+  //     const encrypted = await encrypt(account, req.session.clientPublicJwk)
+  //     res.send(Buffer.from(encrypted))
+  //   }
+  // } else {
+  //   res.send(false)
+  // }
+  res.send(req.session.isLogedIn ? true : false)
 })
 
 async function encrypt(object, jwk) {
