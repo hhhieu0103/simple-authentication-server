07a004961a7741ba2d8ac4e53acde9bb02bc479b
harry2992 <blackcigar2992@gmail.com>
2024-08-17 01:45:10 +0700

    - Refactor authentication route handlers into async functions - Add authentication error responses

--- app.js
+++ app.js
@@ -56,8 +56,10 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  if (req.app.get('env') === 'development') {
+    res.message = err.message;
+    res.error = err;
+  }
 
   // render the error page
   res.status(err.status || 500);
